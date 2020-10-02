8bb5e009f04da9959df0b486d9e0453201bcdecc
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2020-10-02 18:27:36 +1300

    Created window with graphics.

---
+++ compile_for_windows.bat
@@ -0,0 +1,2 @@
+g++ -Iinclude\sdl2 src\main.cpp -Llib -lmingw32 -lsdl2main -lsdl2 -o game.exe
+PAUSE
---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+#include <SDL.h>
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+
+const int w_width = 800, w_height = 600, desired_fps = 60;
+SDL_Window * window;
+SDL_Renderer * rr;
+bool closed = false;
+
+void update() {
+	SDL_Event event;
+	if(SDL_PollEvent(&event)) {
+		if(event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE) {
+			closed = true;
+		}
+	}	
+}
+
+void render() {
+	
+}
+
+void transform(int in[3]) {
+	
+}
+
+int main(int argc, char** argv) {
+	if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
+		std::cout << "COULD NOT INITIALIZE SDL!" << std::endl;
+	}
+	window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w_width, w_height, 0);
+	rr = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	
+	using namespace std::chrono;
+	
+	high_resolution_clock::time_point now = high_resolution_clock::now(), then = now, timer = now;
+	double millisPerUpdate = 1000.0 / desired_fps;
+	double updatesQueued = 0.0;
+	bool canRender = false;
+	
+	int updates = 0, frames = 0;
+	
+	while(!closed) {
+		now = high_resolution_clock::now();
+		duration<double, std::milli> timePassed = now - then;
+		updatesQueued += timePassed.count() / millisPerUpdate;
+		then = now;
+		
+		while(updatesQueued > 0) {
+			update();
+			updatesQueued--;
+			updates++;
+			canRender = true;
+		}
+		
+		if(canRender) {
+			SDL_SetRenderDrawColor(rr, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
+			SDL_RenderClear(rr);
+			//Clears the screen.
+			render();
+			SDL_RenderPresent(rr);
+			frames++;
+			canRender = false;
+		}
+		
+		duration<double, std::milli> timerDuration = now - timer;
+		if(timerDuration.count() >= 5000.0) {
+			timer = now;
+			std::cout << "Updates: " << updates / 5 << ", Frames: " << frames / 5 << std::endl;
+			updates = 0;
+			frames = 0;
+		}
+	}
+	
+	SDL_Quit();
+	
+	return 0;
+}
